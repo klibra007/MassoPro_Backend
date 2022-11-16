d1619b1f6550964de99cf305ff72eee2b33ccef4
Pascal Rocher <info@pascalrocher.com>
2022-11-16 16:49:59 -0500

    Ajout de la requète GET /api/services/:id

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -48,9 +48,13 @@ class ServiceController extends Controller
      * @param  \App\Models\Service  $service
      * @return \Illuminate\Http\Response
      */
-    public function show(Service $service)
+    //public function show(Service $service)
+    public function show($id)
     {
-        //
+        $service = Service::find($id);
+    	return response()->json([
+            'service' => $service
+            ]);        
     }
 
     /**
