ae59531b06372ee29838652cd8216f88e23cde4a
Pascal Rocher <info@pascalrocher.com>
2022-11-18 10:11:58 -0500

    Ajout de la reuète GET /api/durees/:idService

---
+++ app/Http/Controllers/Api/DureeController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Duree;
+use Illuminate\Http\Request;
+
+class DureeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Duree  $duree
+     * @return \Illuminate\Http\Response
+     */
+    public function show($idService)
+    {
+        $durees = Duree::where('idService', '=', $idService)->get();
+        return response()->json([
+            'service' => $durees
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Duree  $duree
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Duree $duree)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Duree  $duree
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Duree $duree)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Duree  $duree
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Duree $duree)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\DureeController;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\UtilisateurController;
 use Illuminate\Http\Request;
@@ -21,6 +22,7 @@ Route::apiResource('services', ServiceController::class)->only([
     'index', 'show'
 ]);
 Route::apiResource('utilisateurs', UtilisateurController::class);
+Route::apiResource('durees', DureeController::class);
 //
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
