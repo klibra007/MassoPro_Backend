c02d749de58312e3a3141dcbca0e864f96b112c8
Pascal Rocher <info@pascalrocher.com>
2022-11-22 12:53:21 -0500

    Ajout de la requète PUT /api/client/:id

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ClientController;
 use App\Http\Controllers\Api\DureeController;
 use App\Http\Controllers\Api\RendezVousController;
 use App\Http\Controllers\Api\ServiceController;
@@ -30,6 +31,7 @@ Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::apiResource('durees', DureeController::class);
 Route::apiResource('servicespersonnels', ServicePersonnelController::class);
 Route::apiResource('rendezvous', RendezVousController::class); //->middleware('auth:sanctum');
+Route::apiResource('client', ClientController::class); //->middleware('auth:sanctum');
 //
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
