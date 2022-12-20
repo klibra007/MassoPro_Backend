d560f2992306175300b9b02571e21d8d325884d3
Pascal Rocher <info@pascalrocher.com>
2022-12-20 16:33:56 -0500

    Ajout d'autres tests

--- tests/Feature/RendezVousTest.php
+++ tests/Feature/RendezVousTest.php
@@ -8,7 +8,58 @@ use Tests\TestCase;
 
 class RendezVousTest extends TestCase
 {
-    public function test_post_rendezvous_false()
+    public function test_post_rendezvous_idService_false()
+    {
+        $data = [
+            'date' => '2022-12-22',
+            'idService' => '999999',
+            'idPersonnel' => '1',
+            'idDuree' => '1'
+        ];
+
+        $response = $this->postJson('/api/rendezvous', $data);
+        $response
+            ->assertStatus(401)
+            ->assertExactJson([
+                'status' => false,
+                'message' => 'Ce service n\'existe pas.'
+            ]);
+    }
+    public function test_post_rendezvous_idPersonnel_false()
+    {
+        $data = [
+            'date' => '2022-12-22',
+            'idService' => '1',
+            'idPersonnel' => '99999',
+            'idDuree' => '1'
+        ];
+
+        $response = $this->postJson('/api/rendezvous', $data);
+        $response
+            ->assertStatus(401)
+            ->assertExactJson([
+                'status' => false,
+                'message' => 'Ce personnel n\'existe pas.'
+            ]);
+    }
+    public function test_post_rendezvous_idDuree_false()
+    {
+        $data = [
+            'date' => '2022-12-22',
+            'idService' => '1',
+            'idPersonnel' => '1',
+            'idDuree' => '99999'
+        ];
+
+        $response = $this->postJson('/api/rendezvous', $data);
+        $response
+            ->assertStatus(401)
+            ->assertExactJson([
+                'status' => false,
+                'message' => 'Cette durée n\'existe pas.'
+            ]);
+    }
+    public function test_post_rendezvous_aucune_plage_horaire_disponible()
     {
         $data = [
             'date' => '2022-12-22',
@@ -25,7 +76,7 @@ class RendezVousTest extends TestCase
                 'message' => 'Aucune disponibilité'
             ]);
     }
-    public function test_post_rendezvous_true()
+    public function test_post_rendezvous_plage_horaire_disponible()
     {
         $data = [
             'date' => '2022-12-22',
